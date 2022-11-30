5bec8bf80de6a3dbcfdd0cf794d73b4fd7b085cf
Petar Jovanovic <pexxxuss@gmail.com>
2022-11-30 20:32:13 +0100

    Ready for deploy

---
+++ src/components/Async.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Async from './Async';
+describe('Async componnet', () => {
+  test('renders posts if request succeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
+    render(<Async />);
+    const listItemElements = await screen.findAllByRole('listitem');
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
